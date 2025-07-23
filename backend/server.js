d4d8d06135adcb13328d5908ae6f79dd6695af9d
AStech01 <ahamedsabith12345@gmail.com>
2025-07-23 16:49:10 +0530

    lst07

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
+
+app.options("*", cors());
 app.use(express.json())
 
 app.use('/api/tasks',taskRoutes)
