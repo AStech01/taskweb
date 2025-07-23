018806b0490fd52b11ff45e2d7113d52dc47a539
AStech01 <ahamedsabith12345@gmail.com>
2025-07-23 16:03:12 +0530

    oke

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -1,6 +1,10 @@
-import axios from "axios";
+// import axios from "axios";
 
+
+// export default axios.create({
+//     baseURL:"http://localhost:5000/api"
+// })
 
 export default axios.create({
-    baseURL:"http://localhost:5000/api"
-})
+    baseURL: import.meta.env.VITE_API_URL
+});
